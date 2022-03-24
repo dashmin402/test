61cdb187fe45966efaa474ffe3e9ebf1c123c7f5
dashmin402 <dashmin402@gmail.com>
2022-03-24 17:41:17 +0800

    Merge branch 'main' of https://github.com/dashmin402/test

--- test2.py
+++ test2.py
@@ -1,8 +1,11 @@
 import cmath as math
+import cv2
 a = math.pi
 b = math.e
 c = a*b
 print('a =',a)
 print('b =',b)
 print('c =',c)
-
+img = cv2.imread('test.jpeg')
+cv2.imshow('test.jpeg', img)
+cv2.waitKey(0)
